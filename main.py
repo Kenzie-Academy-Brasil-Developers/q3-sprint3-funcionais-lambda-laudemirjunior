f8131726840cc1502bfa8c5a22594e4794b21453
Laudemir <nlaudemir@gmail.com>
2022-01-22 16:23:06 -0300

    atividade feita

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Escreva seu código aqui
+from functools import reduce
+
+def characters_filter(list_of_characters, key, value):
+    return list(filter(lambda x: x[key] == value, list_of_characters))
+
+def team_power(list_of_characters):
+    return sum(map(lambda x: x['power'], list_of_characters))
+
+def team_power_2(list_of_characters, advantage):
+    return reduce(lambda acc, num: acc + num['power'], list_of_characters, advantage)
